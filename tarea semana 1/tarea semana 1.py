8ed8b78fa9508493bb323260bfa53bf511c0bfb7
davidmdna <davidmdna12@gmail.com>
2025-01-26 19:02:28 -0600

    Tarea 1.py

---
+++ tarea semana 1/tarea semana 1.py
@@ -0,0 +1,31 @@
+##Ejercicio 1
+#c
+
+##Ejercicio 2
+#Un programa es un conjunto de instrucciones escritas en un lenguaje de programación que una computadora puede interpretar y ejecutar para realizar tareas específicas.
+
+##Ejercicio 3
+#El compilador traduce el codigo fuente a codigo maquina antes de ejecutarlo, el interprete lo hace en tiempo real
+
+##Ejercicio 4
+#d
+
+##Ejercicio 5
+#primt '¡Hola, mundo!' es el error
+
+##Ejercicio 6
+#b
+
+##Ejercicio 7 
+#b) 44
+
+##EJercicio 8
+# Es como el cerebro humano, que procesa y toma decisiones.
+#Similar a la memoria a corto plazo, que almacena información mientras se realiza una tarea.
+#Equivale a una libreta donde anotamos información importante para usarla más tarde.
+#Son como los sentidos (ojos, oídos) que reciben información del entorno.
+#Comparables a la boca o las manos, que permiten expresar o mostrar información.
+#el cerebro
+
+##Ejercicio 9
+#corrigiendo las reglas del lenguaje que se hayan infringido
